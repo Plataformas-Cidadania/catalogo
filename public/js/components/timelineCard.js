0c3bae8be8b573f3718738a9d357068ecadce5c9
Relison Galvão <relison@cd10.com.br>
2022-04-07 14:41:54 -0300

    Timeline

--- public/js/components/timelineCard.js
+++ public/js/components/timelineCard.js
@@ -25,13 +25,13 @@ const Timeline = props => {
       /*let stack1 = document.getElementById('stack1');
       [...stack1.children].reverse().forEach(i => stack1.append(i));
       stack1.addEventListener("click", swap);
-        function swap(e){
+       function swap(e){
           console.log('card');
           console.log(e.target.parentNode.id);
           let card = e.target.parentNode.querySelector(".card:last-child");
           if (e.target !== card) return;
           card.style.animation = "swap 700ms forwards";
-            setTimeout(() => {
+           setTimeout(() => {
               card.style.animation = "";
               e.target.parentNode.prepend(card);
           }, 700);
@@ -41,9 +41,7 @@ const Timeline = props => {
   }, [props.item]);
 
   function swap2(e) {
-    console.log('card timeline');
     e.persist();
-    console.log(e.target.parentNode.id);
     let card = e.target.parentNode.querySelector(".card:last-child");
     if (e.target !== card) return;
     card.style.animation = "swap 700ms forwards";
@@ -117,8 +115,10 @@ const Timeline = props => {
     }, politicas.map((politica, index) => {
       let id = 1;
       return /*#__PURE__*/React.createElement("div", {
-        key: props.item.area + "_politica_" + key.toString() + index,
-        className: "card",
+        key: props.item.area + "_politica_" + key.toString() + index
+        /*className="card"*/
+        ,
+        className: "card cor" + Math.floor(Math.random() * 4),
         onClick: swap2
       }, politica.nome_politica);
     }))));
