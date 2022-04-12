aed93d90e293eae7e09718d6bacf2f3748ea13c3
Bruno Passos <brpassos.programador@gmail.com>
2022-04-12 12:39:58 -0300

    CAT-40

--- packages/cms/resources/views/tipo-politica/_form.blade.php
+++ packages/cms/resources/views/tipo-politica/_form.blade.php
@@ -1,3 +1,3 @@
 <label for="nome">Nome*</label>
-<input type="text" name="nome" class="form-control width-tipo <% validar(politica.nome)%>" ng-model="tipoPolitica.nome" ng-required="true">
+<input type="text" name="nome" class="form-control width-grande <% validar(politica.nome)%>" ng-model="tipoPolitica.nome" ng-required="true">
 <br>
