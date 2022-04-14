63c79e7d30e0ff8877da78df69b90f8da0dcf202
raphael <raphael.s.abreu@gmail.com>
2022-04-14 17:37:55 -0300

    CAT-52;

---
+++ database/migrations/2022_20_04_0011922_alter_area_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterAreaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('catalogo.area', function (Blueprint $table) {
+            $table->string('icone')->nullable();
+            $table->string('resumo',200)->nullable();
+            $table->text('descricao')->nullable();
+        });
+    }
+
+}
