fa68c9f344de2cb49aa4b9ca6d257b99e09649d5
Relison Galvão <relison@cd10.com.br>
2022-04-19 19:01:35 -0300

    CAT-68

--- public/js/components/home/index.js
+++ public/js/components/home/index.js
@@ -1,4 +1,27 @@
 const Home = () => {
+  const {
+    useEffect,
+    useState
+  } = React;
+  useEffect(() => {
+    listData();
+  }, []);
+  const [list, setList] = useState([]);
+  const [divSelected, setDivSelected] = useState(0);
+
+  const listData = async () => {
+    try {
+      const result = await axios.get('json/analise.json');
+      setList(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const clickChart = id => {
+    setDivSelected(id);
+  };
+
   return /*#__PURE__*/React.createElement("div", {
     className: "bg-lgt mt-5 mb-5"
   }, /*#__PURE__*/React.createElement("div", {
@@ -6,24 +29,29 @@ const Home = () => {
   }, /*#__PURE__*/React.createElement("div", {
     className: "row"
   }, /*#__PURE__*/React.createElement("div", {
-    className: "col-md-12 mt-5 mb-5"
-  }, /*#__PURE__*/React.createElement(MixedChart, {
-    id: "mix-chart1",
-    series: [{
-      name: 'TEAM A',
-      type: 'column',
-      data: [23, 11, 22, 27, 13, 22]
-    }, {
-      name: 'TEAM B',
-      type: 'area',
-      data: [44, 55, 41, 67, 22, 43]
-    }, {
-      name: 'TEAM C',
-      type: 'line',
-      data: [30, 25, 36, 30, 45, 35]
-    }],
-    labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '05 Jan 2001']
-  })))));
+    className: "col-md-3 mt-5 mb-5"
+  }, /*#__PURE__*/React.createElement("ul", {
+    className: "menu-left"
+  }, list ? list.map((item, key) => {
+    return /*#__PURE__*/React.createElement("li", {
+      className: "list-group-item-theme  cursor " + (divSelected === key ? 'menu-left-active' : ''),
+      onClick: () => clickChart(key)
+    }, /*#__PURE__*/React.createElement("a", {
+      href: true
+    }, item.titulo));
+  }) : null)), /*#__PURE__*/React.createElement("div", {
+    className: "col-md-9 mt-5 mb-5"
+  }, list ? list.map((item, index) => {
+    return /*#__PURE__*/React.createElement("div", {
+      style: {
+        display: divSelected === index ? 'none' : ''
+      }
+    }, /*#__PURE__*/React.createElement(MixedChart, {
+      id: 'mix-chart' + index,
+      series: item.series,
+      labels: item.labels
+    }));
+  }) : null))));
 };
 
 ReactDOM.render( /*#__PURE__*/React.createElement(Home, null), document.getElementById('home'));
--- resources/js/components/home/index.js
+++ resources/js/components/home/index.js
@@ -1,10 +1,59 @@
 const Home = () => {
+
+    const { useEffect, useState } = React;
+
+    useEffect(() => {
+        listData()
+    }, []);
+
+    const [list, setList] = useState([]);
+    const [divSelected, setDivSelected] = useState(0);
+
+
+    const listData = async () => {
+        try {
+            const result = await axios.get('json/analise.json');
+            setList(result.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const clickChart = (id) => {
+        setDivSelected(id);
+    }
+
+
     return (
         <div className="bg-lgt mt-5 mb-5">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12 mt-5 mb-5" >
-                        <MixedChart id='mix-chart1'  series={[{
+                    <div className="col-md-3 mt-5 mb-5" >
+                        <ul className="menu-left">
+                            {
+                                list ?
+                                    list.map((item, key) => {
+                                        return <li className={"list-group-item-theme  cursor " + (divSelected === key ? 'menu-left-active' : '')} onClick={() => clickChart(key)}>
+                                            <a href>{item.titulo}</a>
+                                        </li>
+                                    })
+                                    :
+                                    null
+                            }
+
+
+                        </ul>
+                    </div>
+                    <div className="col-md-9 mt-5 mb-5" >
+                        {
+                            list ?
+                                list.map((item, index) => {
+                                    return <div style={{display: divSelected === index ? 'none' : ''}}><MixedChart id={'mix-chart'+index}  series={item.series} labels={item.labels}/></div>
+                                })
+                                :
+                                null
+                        }
+                        {/*<MixedChart id='mix-chart1'  series={[{
                             name: 'TEAM A',
                             type: 'column',
                             data: [23, 11, 22, 27, 13, 22]
@@ -16,7 +65,8 @@ const Home = () => {
                             name: 'TEAM C',
                             type: 'line',
                             data: [30, 25, 36, 30, 45, 35]
-                        }]} labels={['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '05 Jan 2001']}/>
+                        }]} labels={['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '05 Jan 2001']}/>*/}
+
                     </div>
                 </div>
             </div>
