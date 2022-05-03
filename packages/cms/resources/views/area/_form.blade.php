7e53ad9c39ffd7e3f4ad7839d9dfe4d87b9d0dc1
Bruno Passos <brpassos.programador@gmail.com>
2022-05-03 12:26:24 -0300

    area

--- packages/cms/resources/views/area/_form.blade.php
+++ packages/cms/resources/views/area/_form.blade.php
@@ -1,3 +1,12 @@
 <label for="nome">Nome*</label>
 <input type="text" name="nome" class="form-control width-grande <% validar(area.nome)%>" ng-model="area.nome" ng-required="true">
 <br>
+
+
+<label for="resumo">Resumo</label>
+<input type="text" name="resumo" class="form-control width-grande <% validar(area.resumo)%>" ng-model="area.resumo" ng-required="false">
+<br>
+
+<label for="resumo">Descrição</label>
+<textarea name="descricao"  class="form-control width-grande <% validar(area.descricao)%>" ng-model="area.descricao" rows="20"></textarea>
+<br>
