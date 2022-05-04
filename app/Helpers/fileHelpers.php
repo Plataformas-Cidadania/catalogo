185c5b0d18c83494aa849214d8d1930e80dba635
raphael <raphael.s.abreu@gmail.com>
2022-05-03 21:10:50 -0300

    CAT-81;

---
+++ app/Helpers/fileHelpers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\File;
+
+if ( ! function_exists('deleteFiles') ) {
+    function deleteFiles(...$files){
+        foreach($files as $file){
+            $filePath = public_path('uploads') . '/' . $file;
+            File::delete($filePath);
+        }
+    }
+}
+
+if ( ! function_exists('createFiles') ) {
+    function createFiles(array $namedFiles, $pretext = false)
+    {
+
+        $res = [];
+        foreach ($namedFiles as $key => $value){
+            if(!$value) continue;
+            if($pretext)
+                $fileNameArq = $key.'_'.time().'.'.$value->extension();
+            else
+                $fileNameArq = time().'.'.$value->extension();
+            $value->move(public_path('uploads'), $fileNameArq);
+            $res[$key] = $fileNameArq;
+        }
+
+        return $res;
+    }
+}
