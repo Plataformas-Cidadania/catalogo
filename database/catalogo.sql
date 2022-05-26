b0bf715a6fc43f06768d763aea8b6937bbcf3f41
raphael <raphael.s.abreu@gmail.com>
2022-05-26 19:24:57 -0300

    CAT-76; bugFix do sequence

--- database/catalogo.sql
+++ database/catalogo.sql
@@ -5241,10 +5241,10 @@ INSERT INTO catalogo.politica_publico_alvo (politica_id, publico_alvo_id) VALUES
 INSERT INTO catalogo.politica_publico_alvo (politica_id, publico_alvo_id) VALUES (661, 16);
 INSERT INTO catalogo.politica_publico_alvo (politica_id, publico_alvo_id) VALUES (662, 3);
 INSERT INTO catalogo.politica_publico_alvo (politica_id, publico_alvo_id) VALUES (663, 17);
-ALTER SEQUENCE catalogo.area_id_seq RESTART WITH 24;
+ALTER SEQUENCE catalogo.area_id_seq RESTART WITH 23;
 ALTER SEQUENCE catalogo.categoria_id_seq RESTART WITH 39;
 ALTER SEQUENCE catalogo.grande_area_id_seq RESTART WITH 7;
-ALTER SEQUENCE catalogo.orgao_id_seq RESTART WITH 140;
+ALTER SEQUENCE catalogo.orgao_id_seq RESTART WITH 141;
 ALTER SEQUENCE catalogo.politica_id_seq RESTART WITH 664;
-ALTER SEQUENCE catalogo.publico_alvo_id_seq RESTART WITH 305;
+ALTER SEQUENCE catalogo.publico_alvo_id_seq RESTART WITH 24;
 ALTER SEQUENCE catalogo.tipo_politica_id_seq RESTART WITH 7;
